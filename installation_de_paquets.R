bc2233054dafae4dbf24442db8418e5c7b1f0f89
Jade Sillère <sjade1925@gmail.com>
2024-10-30 18:27:37 +0100

    Create installation_de_paquets.R

---
+++ installation_de_paquets.R
@@ -0,0 +1,19 @@
+################################################################################
+#                                                                              #
+#             Script pour installer les paquets nécessaires                    #
+#                                                                              #
+################################################################################
+
+install.packages("raster")
+install.packages("sp")
+install.packages("tmap")
+install.packages("sf")
+install.packages("dplyr")
+install.packages("here")
+install.packages("readr")
+install.packages("readxl")
+install.packages("shiny")
+install.packages("leaflet")
+install.packages("RColorBrewer")
+install.packages("rmapshaper")
+install.packages("shinythemes")
